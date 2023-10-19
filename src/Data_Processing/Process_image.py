806edab302ad398a91b9a8ab5e63336f1fe90270
Fatiha maazaz <fatihamaazaz@gmail.com>
2023-10-20 00:39:04 +0100

    Processing and cleaning code

---
+++ src/Data_Processing/Process_image.py
@@ -0,0 +1,9 @@
+from keras.preprocessing import image
+import numpy as np
+
+def process_image(image_path):
+    img = image.load_img(image_path, target_size=(224, 224))
+    img = image.img_to_array(img)
+    img = np.expand_dims(img, axis=0)
+    img = img / 255.0  # Normalize the pixel values
+    return img
